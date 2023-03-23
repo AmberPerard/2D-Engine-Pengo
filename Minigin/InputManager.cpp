cec750180c0d2e6ee5aef44ef510070d609b6b5e
Alynxx <amber.perard@student.howest.be>
2023-03-23 15:54:25 +0100

    Tried adding controller to the engine

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,30 +1,63 @@
 #include <SDL.h>
 #include "InputManager.h"
 
+#include <iostream>
 #include <SDL_syswm.h>
 
 #include <backends/imgui_impl_sdl2.h>
 
 bool dae::InputManager::ProcessInput()
 {
-	//ZeroMemory(&m_CurrentState, sizeof(XINPUT_STATE));
-	//XInputGetState(0, &m_CurrentState);
+	CopyMemory(&m_PreviousState, &m_CurrentState, sizeof(XINPUT_STATE));
+	ZeroMemory(&m_CurrentState, sizeof(XINPUT_STATE));
+	XInputGetState(0, &m_CurrentState);
 
-	SDL_Event e;
-	while (SDL_PollEvent(&e)) {
-		if (e.type == SDL_QUIT) {
-			return false;
-		}
-		if (e.type == SDL_KEYDOWN) {
-			
-		}
-		if (e.type == SDL_MOUSEBUTTONDOWN) {
-			
-		}
-		// etc...
-		//process event for IMGUI
-		ImGui_ImplSDL2_ProcessEvent(&e);
+	auto buttonChanges = m_CurrentState.Gamepad.wButtons ^ m_PreviousState.Gamepad.wButtons;
+	m_ButtonsPressedThisFrame = buttonChanges & m_CurrentState.Gamepad.wButtons;
+	m_ButtonsReleasedThisGame = buttonChanges & (~m_CurrentState.Gamepad.wButtons);
+	//SDL_Event e;
+	//while (SDL_PollEvent(&e)) {
+	//	if (e.type == SDL_QUIT) {
+	//		return false;
+	//	}
+	//	if (e.type == SDL_KEYDOWN) {
+	//		
+	//	}
+	//	if (e.type == SDL_MOUSEBUTTONDOWN) {
+	//		
+	//	}
+	//	// etc...
+
+	//	//process event for IMGUI
+	//	ImGui_ImplSDL2_ProcessEvent(&e);
+	//}
+	// Simply get the state of the controller from XInput.
+	DWORD dwResult = XInputGetState(0, &m_CurrentState);
+
+	if (dwResult == ERROR_SUCCESS)
+	{
+		std::cout << "controller is connected";
+		// Controller is connected
+	}
+	else
+	{
+		// Controller is not connected
 	}
 
 	return true;
+}
+
+bool dae::InputManager::IsButtonDownThisFrame(unsigned button) const
+{
+	return m_ButtonsPressedThisFrame & button;
+}
+
+bool dae::InputManager::IsButtonUpThisFrame(unsigned button) const
+{
+	return m_ButtonsReleasedThisGame & button;
+}
+
+bool dae::InputManager::IsPressed(unsigned button) const
+{
+	return m_CurrentState.Gamepad.wButtons & button;
 }
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -1,12 +1,42 @@
 #pragma once
+#include <vector>
+#include <XInput.h>
+
 #include "Singleton.h"
 
 namespace dae
 {
-	class InputManager final : public Singleton<InputManager>
+	enum class Button
 	{
-	public:
-		bool ProcessInput();
+		ButtonA = XINPUT_GAMEPAD_A,
+		ButtonB = XINPUT_GAMEPAD_B,
+		ButtonX = XINPUT_GAMEPAD_X,
+		ButtonY = XINPUT_GAMEPAD_Y,
+		Up = XINPUT_GAMEPAD_DPAD_UP,
+		Down = XINPUT_GAMEPAD_DPAD_DOWN,
+		Left = XINPUT_GAMEPAD_DPAD_LEFT,
+		Right = XINPUT_GAMEPAD_DPAD_RIGHT,
+		LeftThumb = XINPUT_GAMEPAD_LEFT_THUMB,
+		RightThumb = XINPUT_GAMEPAD_RIGHT_THUMB,
+		LeftShoulder = XINPUT_GAMEPAD_LEFT_SHOULDER,
+		RightShoulder = XINPUT_GAMEPAD_RIGHT_SHOULDER,
+		Start = XINPUT_GAMEPAD_START,
+		Back = XINPUT_GAMEPAD_BACK
 	};
 
+	class InputManager final : public Singleton<InputManager>
+	{
+		XINPUT_STATE m_PreviousState{};
+		XINPUT_STATE m_CurrentState{};
+
+		unsigned int m_ButtonsPressedThisFrame{};
+		unsigned int m_ButtonsReleasedThisGame{};
+	public:
+		bool ProcessInput();
+		bool IsButtonDownThisFrame(unsigned int button) const;
+		bool IsButtonUpThisFrame(unsigned int button) const;
+		bool IsPressed(unsigned int button) const;
+
+
+	};
 }
